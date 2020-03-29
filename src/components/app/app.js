e5e73b78e81543329369aa3dbeb77f3262cf030a
bonial_virto <svetlana.suslova2031@gmail.com>
2020-03-29 21:43:28 +0200

    created delete the post functionality

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import AppHeader from '../app-header/app-header';
 import SearchPanel from '../search-panel/serach-panel';
 import PostStatusFilter from '../post-status-filter/post-status-filter';
@@ -7,30 +7,57 @@ import PostAddForm from '../post-add-form/post-add-form';
 
 import './app.sass';
 
-const App = () => {
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+                data : [
+                0,
+                ['Going to learn React.', 'fhgj'],
+                'ttt',
+                null,
+                {label: 'Going to learn React.', important: true, id: 'fhgj'},
+                {label: 'That is so good!', important: false, id: 'aisl'},
+                {label: 'No boredom during the quarantine...', important: false, id: 'uiuij'}
+            ]
+        }
+    }
 
-    const data = [
-        0,
-        ['Going to learn React.', 'fhgj'],
-        'ttt',
-        null,
-        {label: 'Going to learn React.', important: true, id: 'fhgj'},
-        {label: 'That is so good!', important: false, id: 'aisl'},
-        {label: 'No boredom during the quarantine...', important: false, id: 'uiuij'}
-    ];
+    filteredData = () => {
+        this.setState( ({data}) => {
+            let elements = data.filter((item) => {
+                return  typeof item === 'object' && !Array.isArray(item) && item != null;    
+             });
+             return {
+                 data: elements
+             }
+        });
+        console.log(this.state.data);
+    }
 
-    return (
-        <div className="app">
-            <AppHeader/>
-            <div className="search-panel d-flex">
-                <SearchPanel/>
-                <PostStatusFilter/>
-            </div>
-            <PostList posts={data}/>
-            <PostAddForm/>
-        </div>
-  
-    ) 
+    deleteItem = (id) => {
+        this.filteredData();
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
+            const newArr = [...data.slice(0, index), ...data.slice(index + 1)];
+            return {
+                data: newArr
+            }
+        });
+    } 
+       
+    render() {
+        return (
+            <div className="app">
+                <AppHeader/>
+                <div className="search-panel d-flex">
+                    <SearchPanel/>
+                    <PostStatusFilter/>
+                </div>
+                <PostList posts={this.state.data}
+                onDelete={this.deleteItem}/>
+                <PostAddForm/>
+            </div>      
+        )
+    }    
 }
-
-export default App;
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -3,17 +3,19 @@ import PostListItem from '../post-list-item/post-list-item'
 import { ListGroup } from 'reactstrap';
 import './post-list.sass';
 
-const PostList = ({posts}) => {
+const PostList = ({posts, onDelete}) => {
 
     let elements = posts.filter((item) => {
-        return  typeof item === 'object' && !Array.isArray(item) && item != null;
-         
+        return  typeof item === 'object' && !Array.isArray(item) && item != null;    
      });
+     
      elements = elements.map((item) => {
          const {id, ...itemProps} = item;
          return (
              <li key={id} className="list-group-item">
-                 <PostListItem {...itemProps}/>
+                 <PostListItem 
+                 {...itemProps}
+                 onDelete={ () => onDelete(id) }/>
              </li>
          )
      });
