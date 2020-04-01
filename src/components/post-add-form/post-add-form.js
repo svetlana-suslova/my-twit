2f04cd0f3b797acb41dcfb26fc1bae9b2c8e34fc
bonial_virto <svetlana.suslova2031@gmail.com>
2020-04-01 21:39:06 +0200

    created post adding functionality

--- src/components/post-add-form/post-add-form.js
+++ src/components/post-add-form/post-add-form.js
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Button, Form, Input } from 'reactstrap';
 import './post-add-form.sass';
 
-const PostAddForm = ({onAdd}) => {
-    return (
-    <div className="bottom-panel d-flex">
-        <Input
-            type="text"
-            placeholder="What are your thoughts?"
-            className="new-post-label"/>
-        <Button
-        onClick={() => onAdd('Hello')} 
-        outline color="secondary">Add
-        </Button>
-    </div>
-    ) 
-}
+export default class PostAddForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: ''
 
-export default PostAddForm;
+        }
+    }
+    onValueChange = (e) => {
+        this.setState({
+            text: e.target.value
+        })
+    }
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onAdd(this.state.text);
+        this.setState({
+            text: ''
+        })
+    }
+
+    render() {
+        return (
+            <Form 
+            className="bottom-panel d-flex"
+            onSubmit={this.onSubmit}>
+                <Input
+                    type="text"
+                    placeholder="What are your thoughts?"
+                    className="new-post-label"
+                    onChange={this.onValueChange}
+                    value={this.state.text}/>
+                <Button
+                outline color="secondary">Add
+                </Button>
+            </Form>
+            )
+    }   
+}
